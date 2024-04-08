d59143d6e3deac23d21a2e5b80a7ad94e3c59142
zainmalik <zainmalik1990@gmail.com>
2024-04-08 15:17:06 +0400

    completing a super reptile function

---
+++ 2_Python_Functions/24_returning_a_value_super_reptile.py
@@ -0,0 +1,14 @@
+import turtle
+
+def super_reptile():
+    zain = turtle.Turtle()
+    zain.width(5)
+    zain.color("green")
+    return zain
+
+clark = super_reptile()
+clark.forward(100)
+clark.left(45)
+clark.forward(100)
+
+input()
