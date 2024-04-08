37c6b6298a369e3b470343af39b992559619a387
zainmalik <zainmalik1990@gmail.com>
2024-04-08 17:13:43 +0400

    Drawing 12 polygons of different sides.

---
+++ 2_Python_Functions/28_passing_arguments_in_loops_draw_polygons.py
@@ -0,0 +1,16 @@
+import turtle
+
+def polygon(sides, length):
+  t = turtle.Turtle()
+  t.color("lime")
+  t.speed(0)
+  angle = 360 / sides
+  for side in range(sides):
+    t.forward(length)
+    t.right(angle)
+  t.hideturtle()
+
+for side in [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]:
+   polygon(side, 35)
+
+input()
